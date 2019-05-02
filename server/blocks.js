1951c0500a78a4c247aaa183465dca5d438e1f4a
John BEPPU <john.beppu@gmail.com>
2019-05-02 13:33:09 -0700

    Add ability to wait for log file to exist before tailing
    Also cleaned up a lot of things.
    Used commander to parse command line.

--- server/blocks.js
+++ server/blocks.js
@@ -1,46 +1,80 @@
 const
-  Tail = require('tail').Tail,
-  fs   = require('fs')
+  fs   = require('fs'),
+  Tail = require('tail').Tail
 
-/**
- * Return a function for parsing log lines that's suitable for passing to tail.
- * @param {Array} blocks - an array that will be used for block collection
- * @returns {Function} a function that will parse JSON log lines
- */
-function parseFn(blocks) {
-  var line = 0
-  return function(data) {
-    line++
-    try {
-      let blockData = JSON.parse(data)
-      blocks.push(blockData)
-    }
-    catch (e) {
-      console.warn("JSON parse error on line %d: '%s'", line, data)
-    }
-  }
-}
-
-class Artemis {
+class JSONLogFile {
   /**
-   * Initialize Artemis log file based block collection
+   * Initialize JSON log file based block collection
    * @param {Object} opts - customization
    */
   constructor(opts={}) {
     if (!opts.path) throw new Error("opts.path is required")
     Object.assign(this, opts)
-    let tailOptions = this.tailOptions ? this.tailOptions : { fromBeginning: true }
     this.blocks = []
-    this.tail = new Tail(this.path, tailOptions)
-    this.tail.on('line', parseFn(this.blocks))
-    this.tail.on('error', console.warn)
+    this.startTail()
   }
 
   /**
-   * Start collecting block data.
+   * Wait for file to exist and then start tailing it.
    */
-  start() {
-    this.tail.watch()
+  startTail() {
+    let messageSent = false
+    this.tailWait = setInterval(function(){
+      try {
+        fs.accessSync(this.path)
+        let tailOptions = this.tailOptions ? this.tailOptions : { fromBeginning: true }
+        this.tail = new Tail(this.path, tailOptions)
+        this.tail.on('line', this.parseLineFn(this.blocks).bind(this))
+        this.tail.on('error', console.warn)
+        this.tail.watch()
+        clearInterval(this.tailWait)
+      } catch (err) {
+        if (!messageSent) {
+          console.warn(`waiting for ${this.path} to exist`)
+          messageSent = true
+        }
+      }
+    }.bind(this), 1000)
+  }
+
+  /**
+   * Override this method if the JSON-parsed block data needs to be transformed before storing.
+   * By default, it returns the same object it's passed.
+   * @param {Object} data - JSON-parsed block data
+   * @returns {Object} Transformed block data
+   */
+  transformData(data) {
+    return data
+  }
+
+  /**
+   * Override this method if you want to filter out data.
+   * This runs after `this.transformData(data)`, and
+   * the function should return true if the data should be included and false if not.
+   * @param {Object} data - Data that may be included in `this.blocks` array
+   * @returns {Boolean} True if data should be included
+   */
+  filterData(data) {
+    return true
+  }
+
+  /**
+   * Return a function for parsing log lines that's suitable for passing to tail.
+   * @param {Array} blocks - an array that will be used for block collection
+   * @returns {Function} a function that will parse JSON log lines
+   */
+  parseLineFn(blocks) {
+    var line = 0
+    return function(data) {
+      line++
+      try {
+        let blockData = this.transformData(JSON.parse(data))
+        blocks.push(blockData)
+      }
+      catch (e) {
+        console.warn(`JSON parse error on line %d: '%s' ${e}`, line, data)
+      }
+    }
   }
 
   /**
@@ -48,14 +82,38 @@ class Artemis {
    * @param {Number} n - The block to start from
    */
   blocksSince(n) {
-    this.blocks.filter((item) => {
+    return this.blocks.filter((item) => {
       if (this.isSimulation) return item.index > n && item.index < n + 2
       else return item.index > n
     })
   }
 }
 
+class ArtemisLogFile extends JSONLogFile {
+  /**
+   * Translate some fields so that the React component, Dag, can find what it's looking for.
+   * @param {Object} data - block data from the Artemis log
+   * @returns {Object} transformed block data
+   */
+  transformData(data) {
+    data.headBlockRoot       = data.block_body
+    data.parentHeadBlockRoot = data.block_parent_root
+    //delete data.block_body
+    //delete data.block_parent_root
+    return data
+  }
+
+  /**
+   * Filter out the objects that don't have the necessary fields for rendering
+   * @param {Object} data - block data from the Artemis log
+   * @returns {Boolean} true if the object looks like a block that contains renderable data
+   */
+  filterData(data) {
+    return (data.headBlockRoot && data.parentHeadBlockRoot)
+  }
+}
+
 module.exports = {
-  parseFn,
-  Artemis
+  JSONLogFile,
+  ArtemisLogFile
 }
--- server/server.js
+++ server/server.js
@@ -2,46 +2,38 @@ const
   express = require('express'),
   app     = express(),
   fs      = require('fs'),
-  blocks  = require('./blocks')
+  program = require('commander'),
+  blocks  = require('./blocks'),
+  package = require('../package.json')
+
+let db = undefined
 
 app.get('/api/hello', function (req, res) {
-   res.send('Hello World')
+  res.send('Hello World')
 })
 
 app.get('/api/latest-blocks', function (req, res) {
-   let lastBlock = parseInt(req.query.lastblock)
-   //check that latest block pararmeter is not malformed
-   if(isNaN(lastBlock))res.status(400).send('Bad Request')
+  let lastBlock = parseInt(req.query.lastblock)
+  //check that latest block pararmeter is not malformed
+  if (isNaN(lastBlock)) {
+    res.status(400).send('Bad Request')
+    return
+  }
+  let contents = db.blocksSince(lastBlock)
+  res.json(contents)
+  return
+})
 
-   var path = getPathFromParameters()
-   if(!fs.lstatSync(path).isFile()) new Error('Path parameters for JSON file is either not a file or is malformed')
-   else {
-      let isSim = isSimulation()
-      let contents = (JSON.parse(fs.readFileSync(path)).filter((item) => {
-         if(isSim) return item.index > lastBlock && item.index < lastBlock + 2
-         else return item.index > lastBlock}))
-         //console.log('lastBlock: ' + lastBlock + " nextBlockArraySize: "+ contents.length);
-      res.send(contents)
-   }
- })
+// main
+program
+  .version(package.version)
+  .option('-s, --simulation', 'Simulate a slow stream on /api/latest-blocks')
+  .option('-p, --path [string]', 'Path to JSON log file')
+  .parse(process.argv)
 
-const isSimulation = () => {
-  let isSim;
-  process.argv.forEach(function (val, index, array) {
-    if(val === '-s') isSim = true
-  });
-  return isSim;
-}
-
-const getPathFromParameters = () => {
-  let path;
-  process.argv.forEach(function (val, index, array) {
-    if(val === '-p'){
-      path = process.argv[index+1]
-    }
-  });
-  return path;
-}
+let path = program.path
+let isSimulation = program.simulation
+db = new blocks.ArtemisLogFile({ path, isSimulation })
 
 var server = app.listen(5000, function () {
    var host = server.address().address
